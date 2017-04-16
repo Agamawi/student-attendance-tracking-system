4669f138c1951fe55b30768f2bda5e773f461729
Agamawi <agamawyy@hotmail.com>
2017-04-17 01:42:57 +0300

    small changes

--- sats/static/js/children.js
+++ sats/static/js/children.js
@@ -16,20 +16,3 @@ function reload_children() {
 
     setTimeout(reload_children, (RELOAD_SECONDS * 1000));
 }
-
-function prep_show_modal(id, last_name, first_name, age, grade, section, location, last_seen, school, tag) {
-    var selector_body = $(id);
-    var code = `
-                <p>
-                    <b>Name</b>: `+first_name +` `+last_name+`<br>
-                    <b>Age</b>: `+age+`<br>
-                    <b>Grade</b>: `+grade+`<br>
-                    <b>Section</b>: `+section+`<br>
-                    <b>Location</b>: `+location+`<br>
-                    <b>Last Seen</b>: `+last_seen+`<br>
-                    <b>School</b>: `+school+`<br>
-                    <b>Tag</b>: `+tag+`<br>
-                </p>
-                `;
-    selector_body.html(code);
-}
--- sats/static/js/children_script.js
+++ sats/static/js/children_script.js
@@ -1,3 +1,20 @@
+function prep_show_modal(id, last_name, first_name, age, grade, section, location, last_seen, school, tag) {
+    var selector_body = $(id);
+    var code = `
+                <p>
+                    <b>Name</b>: `+first_name +` `+last_name+`<br>
+                    <b>Age</b>: `+age+`<br>
+                    <b>Grade</b>: `+grade+`<br>
+                    <b>Section</b>: `+section+`<br>
+                    <b>Location</b>: `+location+`<br>
+                    <b>Last Seen</b>: `+last_seen+`<br>
+                    <b>School</b>: `+school+`<br>
+                    <b>Tag</b>: `+tag+`<br>
+                </p>
+                `;
+    selector_body.html(code);
+}
+
 function prep_edit_modal(id, last_name, first_name, age, grade, section, location, last_seen, school, school_id, tag, user_id) {
     var selector_body = $(id);
     var school_code = $("#school_dropdown").html();
