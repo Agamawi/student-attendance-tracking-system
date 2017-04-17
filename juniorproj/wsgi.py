6f8a7ef04af843ed99ae65cab935bef56c5c350d
Agamawi <agamawyy@hotmail.com>
2017-04-17 22:51:05 +0300

    fixed deployment files

--- juniorproj/wsgi.py
+++ juniorproj/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "juniorproj.settings")
 
 application = get_wsgi_application()
+
+if "ONHEROKU" in os.environ:
+	from whitenoise.django import DjangoWhiteNoise
+	application = DjangoWhiteNoise(application)
